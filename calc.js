d763ac19cb5da6813883527b598ed26fccd4e672
ersahii <mehraj95he@gmail.com>
2022-09-07 15:55:27 +0530

    begining of git tut...

---
+++ calc.js
@@ -0,0 +1,10 @@
+let screen = document.getElementById('screen');
+buttons = document.querySelectorAll("button");
+for(items in buttons){
+    item.addEventListener('click,' (e)=>{
+        buttonText = e.target.innerText;
+        if(buttonText=='='){
+            
+        }
+    })
+}
